6e4ae20e783fb1e30ffde68dcfc58603114734f0
isaias <104476799+mrza1127@users.noreply.github.com>
2022-06-15 17:10:11 -0500

    restart

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// start off declaring variables
-$(function () {});
-
-// variable for present day
-var today = moment().format("dddd MMMM Do, YYYY");
-
-// declare workday hours in a variable (?)
-var workday = [
-    {time: "9 AM", event: ""},
-    {time: "10 AM", event: ""},
-    {time: "11 AM", event: ""},
-    {time: "12 PM", event: ""},
-    {time: "1 PM", event: ""},
-    {time: "2 PM", event: ""},
-    {time: "3 PM", event: ""},
-    {time: "4 PM", event: ""},
-    {time: "5 PM", event: ""},
-];
-
-var workSchedule = JSON.parse(localStorage.getItem("workday"));
-if (workSchedule) {
-    workday = workSchedule;
-}
-
-// date for current day
-$("#currentDay").text(today);
-
-// dynamic rows
+// variable to display current date 
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
