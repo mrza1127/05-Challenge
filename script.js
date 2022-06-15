c301c38547153bea389929b28dc94d6d6f45e8bb
isaias <104476799+mrza1127@users.noreply.github.com>
2022-06-15 18:52:53 -0500

    set base variables

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-// variable to display current date 
+// set variable to display current date 
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+// set container class from div as a variable
+var container = $(".container");
+
+// hours into an array
+var workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",];
+var currentHour = parseInt(moment().format("H"))
+
+
