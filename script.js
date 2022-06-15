d1bf1db4992195f47bbcd4935d81bd6b2adec7ae
isaias <104476799+mrza1127@users.noreply.github.com>
2022-06-15 18:58:54 -0500

    use for loop for elements

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ var container = $(".container");
 
 // hours into an array
 var workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",];
-var currentHour = parseInt(moment().format("H"))
+var currentHour = parseInt(moment().format("H"));
+
+// function to show the hour schedule log on the page
+function buildWorkSchedule(hour) {
+    // for loop to create hour rows, task input field, save button
+    for (var i = 0; i < hour.length; i++) {
+        
+    }
+}
 
 
