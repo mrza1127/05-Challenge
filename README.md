a02ba00532347c529feace1f50d53113e1adc441
isaias <104476799+mrza1127@users.noreply.github.com>
2022-06-15 19:43:29 -0500

    updated readme and callback function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Work Day Scheduler - A Calendar App!
+## Work Day Scheduler 
+
+# An easy to use workday planner where you can keep track of hourly tasks. Each time slot will be highlighted in a color depending on whether that hour has past, is the current hour, or is in the future.
--- script.js
+++ script.js
@@ -11,10 +11,39 @@ var currentHour = parseInt(moment().format("H"));
 
 // function to show the hour schedule log on the page
 function buildWorkSchedule(hour) {
-    // for loop to create hour rows, task input field, save button
+    // for loop to create hour rows, task input field, save button (dynamic elements)
     for (var i = 0; i < hour.length; i++) {
-        
-    }
+        let hourRow = $("<div></div>");
+        let workHour = $("<div></div>");
+        let tasks = $("<textarea>");
+        let save = $("<button>");
+        let timeValue = localStorage.getItem(`Task${i}`);
+        let saveIcon = $("<i>");
+
+        // set class & id attributes for previous created element
+        workHour.attr("id", "hour-" + (9 + i));
+        workHour.attr("class", "col-12 col-md-1 hour");
+        hourRow.attr("class", "row time-block");
+        workHour.text(hour[i]);
+
+        tasks.attr("id", "text-" + i);
+        tasks.attr("class", "col-12 col-md-10 past");
+
+        tasks.val(timeValue);
+
+        save.attr("id", "save-" + i);
+        save.attr("class", "col-12 col-md-1 saveBtn");
+
+        // refer to bootstrap for icon
+        saveIcon.attr("class", "");
+    };
+    
+    thisHour();
+}
+
+// function to adapt to the time of day  
+function thisHour () {
+
 }
 
 
